b4ce027d59d4468d27c8527c391c3a0a2ffcdf70
Lawrenz Del Rosario <98245945+thegreatwowens@users.noreply.github.com>
2023-06-15 21:47:43 +0800

    6-16-2023

--- Assets/PlayerManagerScript.cs
+++ Assets/PlayerManagerScript.cs
@@ -3,36 +3,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using PixelCrushers.DialogueSystem;
 
 public class PlayerManagerScript : MonoBehaviour
 {
 
+    public static PlayerManagerScript playerManager;
     [SerializeField]
     private List<GameObject> PlayerGender;
 
     private int genderSet;
     public GameObject currentPlayerObject;
-    Transform SpawnLocation;
-
+    Vector3 SpawnLocation;
+    
     public void FirstSpawn()
     {
         
         genderSet = PlayerPrefs.GetInt("selectedCharacters");
 
-        Instantiate(PlayerGender[genderSet],SpawnLocation);
+        Instantiate(PlayerGender[genderSet],SpawnLocation,new Quaternion());
     }
     Scene currentScene;
-    void Awake()
+    Scene previousScene;
+    void OnEnable()
     {
         SceneManager.sceneLoaded += OnSceneLoaded;
-
     }
 
     private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
     {
+            
             currentScene = SceneManager.GetActiveScene();
+            
             if(currentScene == SceneManager.GetSceneByName("City")){
-                SpawnLocation = GameObject.FindGameObjectWithTag("SpawnPointFromStart").transform;
+                  DialogueManager.ShowAlert("Detroit City");
+                SpawnLocation = GameObject.FindGameObjectWithTag("SpawnPoint").transform.position;
+                FirstSpawn();
+                
             }
 
             
@@ -40,10 +47,11 @@ public class PlayerManagerScript : MonoBehaviour
 
     }
 
-
+ 
 
     void Update()
     {
+
         currentPlayerObject = GameObject.FindGameObjectWithTag("Player");
     }
 }
