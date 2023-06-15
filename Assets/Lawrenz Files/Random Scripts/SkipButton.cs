2fd5f40782bbd0a593846daf1b9cb14dc850267d
Lawrenz Del Rosario <98245945+thegreatwowens@users.noreply.github.com>
2023-06-15 13:59:31 +0800

    Skip Implemented Intro cut Scene

--- Assets/Lawrenz Files/Random Scripts/SkipButton.cs
+++ Assets/Lawrenz Files/Random Scripts/SkipButton.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
+using PixelCrushers.DialogueSystem;
 
 public class SkipButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+public PlayableDirector director;
+public TextMeshProTypewriterEffect effectFirstText;
+
+public TextMeshProTypewriterEffect effectSecondText;
+
+public TextMeshProTypewriterEffect effectThirdText;
+ void Awake() {
+    
+}
+
+
+bool firstSentence,secondSentence,thirdsentence;
+
+public void SkipButtonClicked(){
+    if(!firstSentence){
+
+        director.time = 5.55;
+        effectFirstText.Stop();
+        firstSentence =true;
+    }else if(!secondSentence&& effectFirstText.isPlaying == false){
+        director.time = 16;
+        effectSecondText.Stop();
+        secondSentence =true;
+    }else if(!thirdsentence && effectSecondText.isPlaying == false){
+        director.time = 28;
+        effectThirdText.Stop();
+        thirdsentence = true;
     }
+    
+}
 }
